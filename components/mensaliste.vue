4417ce047117bf58c6ea745c3fdfddea376dcfcf
TwoFingers Catering <s0528279@htw-berlin.de>
2022-09-22 07:38:48 +0200

    created geo location component

--- components/mensaliste.vue
+++ components/mensaliste.vue
@@ -1,5 +1,6 @@
 <template>
-  <!-- you'll need to handle a loading state -->
+  <div class="card p-5">
+     <!-- you'll need to handle a loading state -->
   <div v-if="pending">
     Loading ...
   </div>
@@ -7,12 +8,44 @@
     <div v-for=" mensa, index in mensen " :key="mensa.id">
       <!-- do something -->
       <div v-if="index <= 9">
-        {{ mensa.id }} {{ mensa.city }} {{ index }}
+
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"> 
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">{{ mensa.name }}</p>
+                <p class="subtitle is-6">{{ mensa.city }}</p>
+              </div>
+            </div>
+
+            <div class="content">
+              {{ mensa.address}}
+              
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br>
+              <time datetime="open-status">Open/Closed {{ today }} </time>
+            </div>
+          </div>
+        </div>
+        
       </div>     
     </div>
-    <a class="button p-3">show more</a>
+    <a class="button">show more</a>
 
   </div>
+  </div>
+
+
 </template>
 
 <script setup>
@@ -21,4 +54,7 @@ watch(mensen, (newMensen) => {
   // Because posts starts out null, you won't have access
   // to its contents immediately, but you can watch it.
 })
+
+let today = new Date().toISOString().slice(0, 10)
+
 </script>
---
+++ components/userLocation.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="ui two column centered ">
+      <div class="column">
+        <form class="ui segment large form">
+            <div class="field">
+              <div class="ui right icon input large">
+                <input
+                  type="text"
+                  placeholder="Enter your address"
+                  v-model="address"
+                  ref="autocomplete"
+                />
+                <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+              </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  </template>
+
+  <script setup>
+    //https://medium.com/js-dojo/vue-js-google-maps-api-getting-user-location-f22ad96115ef
+    function locatorButtonPressed() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords.latitude);
+          console.log(position.coords.longitude);
+        },
+        error => {
+          console.log(error.message);
+        },
+        )   
+    }
+  </script>
